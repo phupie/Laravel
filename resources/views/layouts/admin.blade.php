ec8319cbedef6f1e9755c8befb6d96186fb2bc84
YUKI HONDA <pupi.yh@gmail.com>
2021-07-09 07:43:26 +0000

    5th commit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -28,12 +28,32 @@
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navberSupportedContent" aria-expanded="false" aria-label="Togglenavigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-cllapse" id="navbarSupportedContent">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
               
             </ul>
             <ul class="navbar-nav ml-auto">
-              
+            @guest
+              <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+            @else
+              <li class="nav-item dropdown">
+                  <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                      {{ Auth::user()->name }} <span class="caret"></span>
+                  </a>
+
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="{{ route('logout') }}"
+                         onclick="event.preventDefault();
+                                       document.getElementById('logout-form').submit();">
+                          {{ __('Logout') }}
+                      </a>
+
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                          @csrf
+                      </form>
+                  </div>
+              </li>
+            @endguest
             </ul>
           </div>
         </div>
